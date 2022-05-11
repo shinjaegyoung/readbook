40c572dffba0b231b4df05678d883eb628ea55b9
jaegyoung <worudkin@naver.com>
2022-05-11 15:48:22 +0900

    no message

---
+++ app/src/main/java/com/example/readbook/Friend.kt
@@ -0,0 +1,11 @@
+package com.example.readbook
+
+
+    data class Friend(
+        val email: String? = null,
+        val name: String? = null,
+        val profileImageUrl: String? = null,
+        val uid: String? = null
+    )
+
+
