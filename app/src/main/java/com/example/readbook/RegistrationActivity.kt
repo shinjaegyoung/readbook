f87af5d7ebf35d9a3f23f203852b278a64e79423
roro0510 <kgd510@korea.ac.kr>
2022-05-14 03:52:41 +0900

    toast 분리

--- app/src/main/java/com/example/readbook/RegistrationActivity.kt
+++ app/src/main/java/com/example/readbook/RegistrationActivity.kt
@@ -1,14 +1,12 @@
 package com.example.readbook
 
 import android.Manifest
-import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
